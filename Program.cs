1db2f932d827f9933e90b0ac61bc989d39a8e0a7
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-11-26 16:30:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista 001 Exercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variaveis 
+            int a = 2;
+            int b = 3;
+            int c = 6;
+            int soma= a + b + c;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine((a + b) > c);
+            Console.WriteLine("A soma de a+b e menor que c");
+                    
+
+            Console.ReadLine();
+        }
+    }
+}
